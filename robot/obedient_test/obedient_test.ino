1adc713079fb868d217bc9995a1750bbf6070970
vanh17 <vanh17@hanover.edu>
2016-11-17 17:22:34 -0500

    Updated tests for Obedient, stop included. Close #9

--- robot/obedient_test/obedient_test.ino
+++ robot/obedient_test/obedient_test.ino
@@ -17,32 +17,42 @@ void loop() {
   //Test foward function, it will run for 10 second
   obedient.foward();
   obedient.foward();
+  obedient.stopSecond(5);
 
   //Test backward fuction it will run for 15 second
   obedient.foward();
   obedient.foward();
+  obedient.stopSecond(5);
 
   //Test foward function with second
   obedient.fowardSecond(10);
+  obedient.stop();
 
   //Test backward function with second
   obedient.backwardSecond(10);
+  obedient.stop();
 
   //Test turn back from the left
   obedient.turnBackLeft();
+  obedient.stopSecond(2);
 
   //Test turn back from the right
   obedient.turnBackRight();
+  obedient.stop();
 
   //Test turn left
   obedient.turnLeft();
+  obedient.stop();
 
   //Test turn right
   obedient.turnRight();
+  obedient.stop();
 
   //Test circle around from the left
   obedient.circleLeft();
+  obedient.stop();
 
   //Test circle around from the right
   obedient.circleRight();
+  obedient.stop();
 }
