d1a779ef279d583978a88e1c6a8cdb5e055ecd13
vanh17 <vanh17@hanover.edu>
2016-11-17 11:16:35 -0500

    Header file for Obedient

---
+++ robot/Obedient.h
@@ -0,0 +1,21 @@
+/*
+	Obedient.h - Library for directing Obedient robot with movements
+	Created by Hoang N.H. Van, November 14, 2016
+	Released into public domain.
+*/
+
+#ifndef OBEDIENT_H
+#define OBEDIENT_H
+
+#include "Arduino.h"
+	
+class Obedient
+{
+	public:
+		Obedient(int pin);
+		void foward(int second);
+		void backward(int second);
+		void 
+
+};
+#endif
