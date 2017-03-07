cf34e87197c282022a0c2ef2cda6de290ba61de9
Van Nguyen Hung Hoang <vanh17@hanover.edu>
2017-03-06 21:19:35 -0500

    Add comment for how trigger works. #27

--- asr/easy_vr_command/UnitTestFunctionComposite/UnitTestFunctionComposite/function_composite.cpp
+++ asr/easy_vr_command/UnitTestFunctionComposite/UnitTestFunctionComposite/function_composite.cpp
@@ -15,6 +15,10 @@ extern const int G1_TURNBACKRIGHT = 5;
 extern const int G1_STOP = 6;
 extern int group = -1;
 
+/* This is the function that handle the trigger to start the Obedient
+* This is only work if we passed the right idx, which is 0, and not working
+* at all for other cases
+*/
 int trigger(int idx) {
 	switch (idx) {
 	    case G0_OBEDIENT:
--- asr/easy_vr_command/UnitTestFunctionComposite/UnitTestFunctionComposite/function_composite.h
+++ asr/easy_vr_command/UnitTestFunctionComposite/UnitTestFunctionComposite/function_composite.h
@@ -1,20 +1,10 @@
 #ifndef _FUNCTION_COMPOSITE_H_
 #define _FUNCTIONC_COMPOSITE_H_
-//group
-extern const GROUP_0 = 0;
-extern const GROUP_1 = 1;
 
-//command idx
-extern const int G0_OBEDIENT = 0;
-extern const int G1_FOWARD = 0;
-extern const int G1_BACKWARD = 1;
-extern const int G1_LEFTTURN = 2;
-extern const int G1_RIGHTTURN = 3;
-extern const int G1_TURNBACKLEFT = 4;
-extern const int G1_TURNBACKRIGHT = 5;
-extern const int G1_STOP = 6;
-extern int const group = -1;
-
+/* This is the function that handle the trigger to start the Obedient
+ * This is only work if we passed the right idx, which is 0, and not working 
+ * at all for other cases
+ */
 int trigger(int idx);
 
 #endif
