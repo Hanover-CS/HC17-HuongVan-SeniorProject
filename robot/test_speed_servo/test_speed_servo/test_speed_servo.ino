a67f5ea189c19879a956a0dbbdf9b6e3358f5d6d
Van Nguyen Hung Hoang <vanh17@hanover.edu>
2017-03-24 21:46:18 -0400

    Added loop function to run the test. #59
    Adding loo function to run the test over and over again for recording
    the speed corresponding to each pulse.

--- robot/test_speed_servo/test_speed_servo/test_speed_servo.ino
+++ robot/test_speed_servo/test_speed_servo/test_speed_servo.ino
@@ -19,25 +19,51 @@
  * Take no argument and return nothing
  */
 void setup() {
-  // put your setup code here, to run once:
-  // Make some sounds here to make sure a system works fine.
-  tone(5, 1000, 1000);
-  delay(1000);
+    // put your setup code here, to run once:
+    // Make some sounds here to make sure a system works fine.
+    tone(5, 1000, 1000);
+    delay(1000);
 
-  // Print out testing information to the serial
-  Serial.begin(9600);
+    // Print out testing information to the serial
+    Serial.begin(9600);
 
-  // Attach servos to right port
-  servoLeft.attach(10);
-  servoRight.attach(11);
+    // Attach servos to right port
+    servoLeft.attach(10);
+    servoRight.attach(11);
 
 }
 
 /*
- * 
+ * Loop function - will run everything in here and then start another loop and keep running until
+ * the program is ended.
+ * Putting thing that you want to run over and over again in here.
+ * Takes no arguments and return nothing
  */
 void loop() {
-  
+    for (int pulse = 1375; pulse <= 1625; pulse += 25) {
+        // Display pulse value for recording
+        Serial.print("pulse = ");
+        Serial.println(pulse);
+
+        // Waiting for the start announcement
+        Serial.println("Press a key and click");
+        Serial.println("Send to start servo...");
+
+        // Will do nothing if the user has not press any key
+        while(Serial.available() == 0) {
+            Serial.read();  
+        }
+
+        // When the user press the key, start testing each of the servo one at a time
+        Serial.println("Running...");
+        servoLeft.writeMicroseconds(pulse);
+        //servoRight.writeMicrosecond(pulse);
+        delay(6000)
+
+        // stop the servo after the test.
+        servoLeft.writeMicroseconds(1500);
+        //servoRight.writeMicroseconds(1500);
+    }
   
 
 }
