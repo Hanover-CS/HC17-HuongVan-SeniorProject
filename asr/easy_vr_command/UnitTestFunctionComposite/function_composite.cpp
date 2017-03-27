1d0e40b3e08d6e76c0fb6ce8f8d2e676d21db443
Van Nguyen Hung Hoang <vanh17@hanover.edu>
2017-03-27 11:34:19 -0400

    Added pickUnit to test file. #13

--- asr/easy_vr_command/UnitTestFunctionComposite/function_composite.cpp
+++ asr/easy_vr_command/UnitTestFunctionComposite/function_composite.cpp
@@ -298,3 +298,20 @@ int sensorOrBasicOrList(int choice) {
             break;   
     }
 }
+
+/* This is the function that determine what unit we will use for function in advanced group
+*     0 - all functions work with unit of seconds
+*     1 - all functions work with unit of metric, in this case is inch
+*  @param {integer} idx - the idx of the command that the shield recognized.
+*  The function will return nothing, but set `unit` variable to corresponding unit.
+*/
+int pickUnit(int idx) {
+    // Make a sound so that users know shield recognize their words
+    tonePlay(1600, 1000);  
+    // Set up the unit to the right unit  
+    unit = idx;
+    // Get back to the advanced group so that we can call function with the right unit.
+    group = GROUP_2;
+
+    return unit; 
+}
